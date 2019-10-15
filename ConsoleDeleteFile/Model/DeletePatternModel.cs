8662e321e5ba267a08ccb172fa8ce43e76ea7d89
ace.lee <ace.lee@quenn.com>
2019-10-15 14:48:14 +0800

    commit models

---
+++ ConsoleDeleteFile/Model/DeletePatternModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleDeleteFile.Model
+{
+    public class DeletePatternModel
+    {
+        public string Path { set; get; }
+
+        public string DeleteFilePatern { set; get; }
+
+        public bool IsRecursive { set; get; }
+
+        public TimeSpan KeepTimeSpan { set; get; }
+    }
+}
---
+++ ConsoleDeleteFile/Model/InputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleDeleteFile.Model
+{
+    public class InputModel
+    {
+        public List<DeletePatternModel> DeleteModels { set; get; }
+    }
+}
